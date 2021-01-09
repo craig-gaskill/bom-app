955f5168c18ac5519dcd62ae7a503465cafd7bf2
Craig Gaskill <craig.gaskill@gmail.com>
2021-01-09 16:51:16 -0600

    Upgraded to Angular 11

--- src/app/feature/settings/dictionaries/dictionaries.component.spec.ts
+++ src/app/feature/settings/dictionaries/dictionaries.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {DictionariesComponent} from './dictionaries.component';
 
@@ -6,7 +6,7 @@ describe('DictionariesComponent', () => {
   let component: DictionariesComponent;
   let fixture: ComponentFixture<DictionariesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DictionariesComponent ]
     })
